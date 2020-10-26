bfcf2513720f7e5a0018bcf34d534fc82bb8399e
Aryanjj <69970051+Aryanjj@users.noreply.github.com>
2020-10-26 13:38:26 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,23 @@
+class Plinko {
+    constructor(x, y, radius) {
+        var options = {
+            isStatic: true,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 4
+        }
+        this.radius = 10;
+        this.x = x;
+        this.y = y;
+        this.body = Bodies.circle(this.x, this.y, this.radius, options);
+
+
+        World.add(world, this.body);
+    }
+    display() {
+        // circleMode(CENTER);
+        fill("white");
+        ellipse(this.body.position.x, this.body.position.y, this.radius);
+    }
+
+}
